2b1ea58ca95d4e09c086dc5dbe42aa0066f4e981
George <george_bergson@hotmail.com>
2023-08-22 17:32:24 -0400

    Aula 2 - Verificando argumento e impressao

---
+++ Aula02/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args){
+        Console.WriteLine("Segunda Aula");
+        if(args.Length>0){
+            Console.WriteLine(args.GetValue(0));
+        }
+    }
+}
